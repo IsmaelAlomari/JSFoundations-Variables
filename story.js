b2e811d88c8a5a551308564bdb5e062bab7ff93d
IsmaelAlomari <ismaelalomari994@gmail.com>
2021-05-17 17:15:47 +0300

    solved

---
+++ story.js
@@ -0,0 +1,7 @@
+let name = "ismael"
+let age = 18
+let hobby = "footballer"
+
+
+
+console.log(`Mr. ${name}, in 33 years, you will be ${age+33} years old. By then you will have decades of ${hobby} practice!`)
